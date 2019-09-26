e743bad6b774c7024c85cfea3b794c4332f6c8b8
Dieter Verhelst <info@werus.be>
2019-09-26 22:25:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+[![Build Status](https://travis-ci.com/fiaasco/docker.svg?branch=master)](https://travis-ci.com/fiaasco/docker)
+
+# Ansible Role: docker
+
+This role installs docker-ce and docker-compose for easy management.
+Older legacy docker packages will be removed
+
+## Requirements
+
+
+
+## Role Variables
+
+
+Role variables are documented inline in the following files:
+- Required variables in meta/main.yml
+- Optional variables in defaults/main.yml
+- Constants in vars/main.yml
+
+
+## Dependencies
+
+
+## Examples
+
+Include the role in your playbook:
+
+```
+    - hosts: servers
+      roles:
+         - { role: docker }
+```
+
+## Tags
+
+No tags available.
+
+## License
+
+MIT
+
+## Further Reading
+
+
+
+## Author Information
+
+Dieter Verhelst
---
+++ molecule/centos/tests/test_docker.py
@@ -0,0 +1,35 @@
+import os
+import pytest
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('pkg', ['docker-ce', 'containerd.io', 'docker-ce-cli'])
+def test_docker_packages(host, pkg):
+    assert host.package(pkg).is_installed
+
+
+def test_docker_compose(host):
+    compose = host.file('/usr/local/bin/docker-compose')
+    assert compose.exists
+    assert compose.user == 'root'
+    assert compose.group == 'root'
+
+
+def test_docker_compose_exec(host):
+    command = host.run("/usr/local/bin/docker-compose version")
+    assert command.rc == 0
+    assert "docker-compose" in command.stdout
+    assert "docker-py" in command.stdout
+
+
+def test_docker_hello_world(host):
+    command = host.run("docker run hello-world")
+    assert "This message shows that your installation appears to be working correctly." in command.stdout
+
+
+def test_docker_service(host):
+    assert host.service('docker').is_enabled
+    assert host.service('docker').is_running
